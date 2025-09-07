de3aabab7e1fcf226418a12a9cafe24efa7615cf
Valentin Colombat <valentincolombat@gmail.com>
2025-09-07 18:44:48 +0200

    Ajout du calcul des résultats des dés - V

--- diceRoller.js
+++ diceRoller.js
@@ -16,21 +16,39 @@ function rollRandomDice(max){
 return Math.floor(Math.random()* max) +1
 }
 
-console.log(rollRandomDice(6))
-
 // ajouter un écouteur d'évènement : lorsque l'utilisateur saisi une ligne et appui sur entrée (input) 
 
 rl.on('line', (input) => {
     //on stocke le résultat dans une constante 
     // ajout trim() et toLowerCase pour supprimer les espaces et les majuscules
     const commande = input.toLowerCase().trim()
-    console.log (`Vous avez saisi ${commande}`)
-
-})
 
 
+        if (commande === 'help') {
+        console.log(`Indiquez un nombre pour lancer des dés, ou "bye" pour quitter.`);
 
+        } else if (commande === 'bye') {
+            rl.close();
 
+        } // gérer le cas où l'utilisateur ne tape rien ou autre chose qu'un nombre 
+        else if ( commande === ' ' || isNaN(commande) === (commande)){
+            console.log(`Erreur: commande non reconnue. Indiquez un nombre pour lancer des dés, ou 'bye' pour quitter.`)
 
-// if (commande === 'help') {
-//      console.log(`Indiquez un nombre pour lancer des dés, ou "bye" pour quitter.`);
+        } else {
+
+            const nombreDeDes = Number(commande)
+            let somme = 0;
+
+            //boucler suivant le nombre de lancés 
+
+            for (let i=0; i<nombreDeDes; i++){
+                const resultat = rollRandomDice(6)
+                // il faut ajouter les résultats les uns aux autres 
+                console.log(resultat)
+                somme += resultat
+        }
+        
+        
+        console.log(`La somme des ${nombreDeDes} dés fait ${somme}.`)
+
+}});
